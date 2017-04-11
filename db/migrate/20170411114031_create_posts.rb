cc6b7cc8c41153aa6caa19d46ab174705cc04c18
dahye Jeong <wjdekgp1750@naver.com>
2017-04-11 20:43:01 +0900

    Scaffolding Post

---
+++ db/migrate/20170411114031_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :local
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
