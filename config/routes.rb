691e320aea2ae4f179d965491f837bbda1b6bc21
dahye Jeong <wjdekgp1750@naver.com>
2017-04-18 14:07:41 +0900

    navbar 구조수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {:omniauth_callbacks => 'omniauth_callbacks', :registrations => 'registrations'}
+
   resources :posts do
     collection do
      get 'search'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'lighting'=>'users#index'
   root 'posts#index'
