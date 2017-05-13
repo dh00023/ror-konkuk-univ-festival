6febff2f62adffb76307f2e2f4fe0d3cb86b59a6
project42da <cmw9101@konkuk.ac.kr>
2017-05-14 00:44:49 +0900

    아 뭐지

--- db/migrate/20170411114031_create_posts.rb
+++ db/migrate/20170411114031_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.0]
       t.string :name
       t.float :x
       t.float :y
+      t.string :dn
       t.string :address #대력적인 위치 직접입력
       t.string :category #귀여운 이런거
       t.string :feature #안주쌈 이런거
